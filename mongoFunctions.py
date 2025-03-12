dcde27aa8ed8e3d8f692af1cd204af357d1b403a
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-03-12 14:43:08 +0100

    changed the insert function

--- mongoFunctions.py
+++ mongoFunctions.py
@@ -1,15 +1,26 @@
 from mongo import *
 
 
-def insert(user, period, func, args):
+def insert(user, interval, func, args, lastProcess, nextProcess):
+    """
+
+    :param user: Telegram user id
+    :param interval: interval in which to execute function e. 1 day, 1 week,...
+    :param func: function to execute
+    :param args: arguments to pass to function
+    :param lastProcess: time of the last function process
+    :param nextProcess: calculated time of the next process
+    :return:
+    """
     DB = db_connect()
     collection = DB[""]
 
     data = {
         "userId": user,
-        "period": period,
         "function": func,
-        "arguments": args
+        "arguments": args,
+        "interval": interval,
+        "lastProcess": lastProcess
     }
 
     insertedId = collection.insert_one(data).inserted_id
