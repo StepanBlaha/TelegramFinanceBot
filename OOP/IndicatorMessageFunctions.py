ed35cc1b5404dee34819113a868e652aa1f8562d
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-04-27 21:52:57 +0200

    indicatormsg function fixes

--- OOP/IndicatorMessageFunctions.py
+++ OOP/IndicatorMessageFunctions.py
@@ -19,23 +19,23 @@ class IndicatorMessage:
     async def send_atr(self, update, symbol):
         await update.message.reply_text(f"The ATR for {symbol}: {self.indicators.get_atr(symbol)}.")
 
-    async def send_rsi(self, update, symbol):
-        await update.message.reply_text(f"The RSI for {symbol} for the last 14 days: {self.indicators.get_rsi(symbol)}.")
+    async def send_rsi(self, update, symbol, days=14):
+        await update.message.reply_text(f"The RSI for {symbol} for the last {days} days: {self.indicators.get_rsi(symbol)}.")
 
-    async def send_avl(self, update, symbol):
-        await update.message.reply_text(f"The AVL for {symbol} for the last 14 days: {self.indicators.get_avl(symbol)}.")
+    async def send_avl(self, update, symbol, days=14):
+        await update.message.reply_text(f"The AVL for {symbol} for the last {days} days: {self.indicators.get_avl(symbol)}.")
 
     async def send_boll(self, update, symbol):
         boll_data = self.indicators.get_boll(symbol, dictionary=True)
         await update.message.reply_text(
             f"The bollinger lines for {symbol}:\n  middle band: {boll_data['MB']}\n  lower band: {boll_data['LB']}\n  upper band: {boll_data['UB']}")
 
-    async def send_ema(self, update, symbol):
-        await update.message.reply_photo(photo=self.plot.plot_ema(symbol, 14), caption=f"EMA chart for {symbol} over 14 days."),
-        await update.message.reply_text(f"Here is the EMA data:\n```\n{self.dataframe.get_ema_dataframe(symbol, 14)}\n```",
+    async def send_ema(self, update, symbol, days=14):
+        await update.message.reply_photo(photo=self.plot.plot_ema(symbol, days), caption=f"EMA chart for {symbol} over {days} days."),
+        await update.message.reply_text(f"Here is the EMA data:\n```\n{self.dataframe.get_ema_dataframe(symbol, days)}\n```",
                                         parse_mode="Markdown")
 
-    async def send_kdj(self, update, symbol):
-        await update.message.reply_photo(photo=self.plot.plot_kdj(symbol, 14), caption=f"KDJ chart for {symbol} over 14 days.")
-        await update.message.reply_text(f"Here is the KDJ data:\n```\n{self.dataframe.get_kdj_dataframe(symbol, 14)}\n```",
+    async def send_kdj(self, update, symbol, days=14):
+        await update.message.reply_photo(photo=self.plot.plot_kdj(symbol, days), caption=f"KDJ chart for {symbol} over {days} days.")
+        await update.message.reply_text(f"Here is the KDJ data:\n```\n{self.dataframe.get_kdj_dataframe(symbol, days)}\n```",
                                         parse_mode="Markdown")
